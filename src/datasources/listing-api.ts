fa6ab8fa8365ab3c1bd5744bf66e71d05d58fd3a
Vincent <vincentflores88@gmail.com>
2026-03-02 14:23:55 +0800

    done

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,5 +1,5 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
 
@@ -11,5 +11,15 @@ export class ListingAPI extends RESTDataSource {
 
   getListing(listingId: string): Promise<Listing> {
     return this.get<Listing>(`listings/${listingId}`);
+  }
+
+  getAmenities(listingId: string): Promise<Amenity[]> {
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+
+  createListing(listing: CreateListingInput): Promise<Listing> {
+    return this.post<Listing>("listings", {
+      body: { listing }
+    });
   }
 }
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from './types'
+import { validateFullAmenities } from "./helpers";
 
 export const resolvers: Resolvers = {
   Query: {
@@ -7,6 +8,33 @@ export const resolvers: Resolvers = {
     },
     listing: (_, { id }, { dataSources }) => {
       return dataSources.listingAPI.getListing(id);
+    }
+  },
+  Listing: {
+    amenities: ({ id, amenities }, _, { dataSources }) => {
+      return validateFullAmenities(amenities)
+        ? amenities
+        : dataSources.listingAPI.getAmenities(id);
+    }
+  },
+  Mutation: {
+    createListing: async (_, { input }, { dataSources }) => {
+      try {
+        const response = await dataSources.listingAPI.createListing(input);
+        return {
+          code: 200,
+          success: true,
+          message: "Listing successfully created!",
+          listing: response
+        };
+      } catch (err) {
+        return {
+          code: 500,
+          success: false,
+          message: `Something went wrong: ${err.extensions.response.body}`,
+          listing: null
+        };
+      }
     },
   },
 };
